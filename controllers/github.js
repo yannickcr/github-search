45f3af3731abce6e255e290cbdb9b4077b5e8f75
Yannick Croissant <k1der.country@free.fr>
2011-08-07 23:03:57 +0200

    add graphs on the repo page switch to the strobe template engine (node-t is useless since he do not have filters)

--- controllers/github.js
+++ controllers/github.js
@@ -21,6 +21,14 @@ query = function(path, callback){
 	});
 }
 
+resetTime = function(date){
+	date.setHours(0);
+	date.setMinutes(0);
+	date.setSeconds(0);
+	date.setMilliseconds(0);
+	return date;
+}
+
 /*
  * Render the home page
  */
@@ -43,7 +51,50 @@ search = function(req, res, search){
 repo = function(req, res, user, repo){
 	query('/api/v2/json/repos/show/' + user + '/' + repo + '/contributors', function(data){
 		query('/api/v2/json/commits/list/' + user + '/' + repo + '/master', function(data2){
-			data.commits = data2.commits;
+			
+			/*
+			 * Commits graph
+			 */
+			data.commits = {};
+			data.commitMax = 0;
+			var lastCommit = resetTime(new Date(data2.commits[0].committed_date)).getTime(),
+				firstCommit = resetTime(new Date(data2.commits[data2.commits.length - 1].committed_date)).getTime();
+			
+			// Init the empty commit array
+			for (var i = lastCommit, j = firstCommit; i >= j; i = i - 86400000){
+				var fullDate = new Date(i),
+					day = fullDate.getDate() + '/' + (fullDate.getMonth() + 1) + '/' + fullDate.getFullYear();
+					
+				data.commits[day] = 0;
+			}
+			
+			// Count the commits by day
+			for (var i = 0, j = data2.commits.length; i < j; i++){
+				var fullDate = new Date(data2.commits[i].committed_date),
+					day = fullDate.getDate() + '/' + (fullDate.getMonth() + 1) + '/' + fullDate.getFullYear(),
+					current = resetTime(fullDate).getTime();
+					
+				data.commits[day]++;
+				
+				if(data.commits[day] > data.commitMax) data.commitMax = data.commits[day];
+			}
+			
+			/*
+			 * Contributors graph
+			 */
+			data.contributions = {
+				labels: [],
+				commits:[]
+			}
+			for (var i = 0, j = data.contributors.length, others = 0; i < j; i++){
+				if (i < 6) {
+					data.contributions.labels.push(data.contributors[i].name);
+					data.contributions.commits.push(data.contributors[i].contributions);
+				} else others += data.contributors[i].contributions;
+			}
+			data.contributions.labels.push('Others');
+			data.contributions.commits.push(others);
+			
 			render('github/repo.html', {data: data}, res);
 		});
 	});
--- index.js
+++ index.js
@@ -6,26 +6,40 @@ http = require('http'),
 net = require('net'),
 fs = require('fs'),
 util = require('util'),
-tpl = require('node-t'),
+tpl = require('strobe'),
 router = require('choreographer').router(),
 mime = require('mime').mime,
 //cradle = require('cradle'),
-markdown = require('markdown'),
+//markdown = require('markdown'),
 
 require('./urls');   // Routing file
 
 // Init Templates
-tpl.init('./templates', true);
+tpl.setTemplatesDir('./templates');
+tpl.setDebug(true);
 
 /*tpl.Filters.markdown = function(string){
   return markdown.toHTML( string );
 }*/
 
+tpl.Filters.join = function(array, glue){
+	if(array.join) return array.join(glue);
+	var data = [];
+	for (var i in array) data.push(array[i]);
+	return data.join(glue);
+}
+
 render = function(template, data, res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end(
-		tpl.fromFile(template).render(data)
-	);
+  new tpl.Template(template).load(function(err, template) {
+    if (err) sys.puts(err);
+    else template.render(data, function(err, output) {
+      if (err) sys.puts(err);
+      else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(output.join(''));
+      }
+    });
+  });
 }
 
 // Init Database
